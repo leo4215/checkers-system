ff40c9b5df1ce448858829d183438614017174e7
Leonardo de Camargo <leonardo4215@live.com>
2024-07-10 17:03:42 -0400

    Improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,30 @@
 package application;
 
 import checkers.CheckersPosition;
+import checkers.Color;
 
 public class UI {
-    
+
+    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void printBoard(CheckersPosition[][] mat) {
         for (int i = 0; i < mat.length; i++) {
             System.out.print((8 - i) + " ");
@@ -18,8 +39,10 @@ public class UI {
     public static String printPosition(CheckersPosition position) {
         if (position.getCheckersPiece() == null) {
             return " -";
+        } else if (position.getCheckersPiece().getColor() == Color.WHITE) {
+            return " " + ANSI_WHITE + position.getCheckersPiece() + ANSI_RESET;
         } else {
-            return " " + position.getCheckersPiece();
+            return " " + ANSI_YELLOW + position.getCheckersPiece() + ANSI_RESET;
         }
     }
 }
